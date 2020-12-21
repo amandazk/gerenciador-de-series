4794521ba9bb02d53af678a93870b7b11a8c6db8
amandazk <zacharukaz@gmail.com>
2020-12-21 17:49:12 -0300

    Rotas nomeadas e novos ícones

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -33,7 +33,7 @@ class SeriesController extends Controller
                 "Série {$serie->id} criada com sucesso {$serie->nome}"
             );
 
-        return redirect('/series');
+        return redirect()->route('listar_series');
     }
 
     public function destroy(Request $request)
@@ -44,7 +44,7 @@ class SeriesController extends Controller
                 'mensagem',
                 "Série removida com sucesso"
             );
-        
-        return redirect('/series');
+
+        return redirect()->route('listar_series');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,9 @@ use App\Http\Controllers\SeriesController;
 |
 */
 
-Route::get('/series', [SeriesController::class, 'index']);
-Route::get('/series/criar', [SeriesController::class, 'create']);
+Route::get('/series', [SeriesController::class, 'index'])
+    ->name('listar_series');
+Route::get('/series/criar', [SeriesController::class, 'create'])
+    ->name('form_criar_serie');
 Route::post('/series/criar', [SeriesController::class, 'store']);
-Route::delete('/series/{id}', [SeriesController::class, 'destroy']);
+Route::delete('/series/{id}', [SeriesController::class, 'destroy']); 
