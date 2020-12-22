25e7e929a5213d5d2a086312d8309a63260aa836
amandazk <zacharukaz@gmail.com>
2020-12-22 11:12:22 -0300

    Formulário para temporadas e episódios

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -6,7 +6,8 @@
 
 @section('conteudo')
     @if ($errors->any())
-        <div class="alert alert-danger"> {{-- se existir algum erro, exibe essa div --}}
+        <div class="alert alert-danger"> {{-- se existir algum erro, exibe essa div
+            --}}
             <ul>
                 @foreach ($errors->all() as $error)
                     <li>{{ $error }}</li>
@@ -14,13 +15,26 @@
             </ul>
         </div>
     @endif
+
     <form method="post">
         @csrf
-        <div class="form-group">
-            <label for="nome">Nome</label>
-            <input type="text" class="form-control" name="nome" id="nome">
+        <div class="row">
+            <div class="col col-8">
+                <label for="nome">Nome</label>
+                <input type="text" class="form-control" name="nome" id="nome">
+            </div>
+
+            <div class="col col-2">
+                <label for="nome">Nº Temporadas</label>
+                <input type="number" class="form-control" name="qtd_temporadas" id="qtd_temporadas">
+            </div>
+
+            <div class="col col-2">
+                <label for="nome">Ep. por Temporada</label>
+                <input type="number" class="form-control" name="ep_por_temporada" id="ep_por_temporada">
+            </div>
         </div>
 
-        <button class="btn btn-primary">Adicionar</button>
+        <button class="btn btn-primary mt-2">Adicionar</button>
     </form>
 @endsection
